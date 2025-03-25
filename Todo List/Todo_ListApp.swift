2cb9a4f5e6fad491a2c72207b28c686aba046ed5
spaceman1412 <spacemannormal@gmail.com>
2025-03-25 14:03:57 +0700

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by H470-088 on 25/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
